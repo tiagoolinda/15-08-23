af48e4476a2639147471c369ed46e833bf34e748
tiagoolinda <138938543+tiagoolinda@users.noreply.github.com>
2023-08-16 22:23:04 -0300

    Add files via upload

--- 15-08-23/Sistema de Cadastro/sistema.js
+++ 15-08-23/Sistema de Cadastro/sistema.js
@@ -10,7 +10,7 @@ function menu() {
         "4 - Sair do sistema \n"
     ));
 
-    switch (opca0) {
+    switch (opcao) {
         case 1:
             cadastrar_cliente();
             break
@@ -33,21 +33,73 @@ function menu() {
 function cadastrar_cliente() {
     let x = "n";
     relatorio = "*- Relatório de Cliente -* <br> ";
-}
-while (x.toupperCase() != "S") {
+
+while (x.toUpperCase() != "S") {
     id = parseInt(prompt("Digite o id do Cliente"));
-    nome = parseInt(prompt("Digite o nome do Cliente"));
-    endereco = parseInt(prompt("Digite o endereço do Cliente"));
-    relatorio = relatorioa + "<br>+-----------------------------------------------------------+<br><br>"
+    nome = prompt("Digite o nome do Cliente");
+    endereco = prompt("Digite o endereço do Cliente");
+    relatorio = relatorio + "<br>+-----------------------------------------------------------+<br><br>"+
     "ID : " + id + "<br>" +
-    "NOME : " + nome + "<br>"
+    "NOME : " + nome + "<br>"+
     "ENDERECO : " + endereco + "<br>+-----------------------------------------------------------+<br><br>"
 
     confirme = prompt("Finalizar Cadastro - S para sim ou N - Não?");
-    if (conforme.toupperCase() == "S") {
+    if (confirme.toUpperCase() == "S") {
         x = "S";
     };
 
     document.getElementById("relatorio").innerHTML = "<p>" + relatorio + "<p>";
+}
 
+}
+
+/* Função cadastrar colaborador */
+
+function cadastrar_colaborador() {
+    let x = "n";
+    relatorio = "*- Relatório de Colaborador -* <br> ";
+
+    while (x.toUpperCase() != "S") {
+        id = parseInt(prompt("Digite o cracha do colaborador"));
+        nome = prompt("Digite o nome do Colaborador");
+        funcao = prompt("Digite a função do Colaborador");
+        relatorio = relatorio + "<br>+-----------------------------------------------------------+<br><br>"+
+        "ID : " + id + "<br>" +
+        "NOME : " + nome + "<br>"+
+        "FUNCAO : " + funcao + "<br>+-----------------------------------------------------------+<br><br>"
+    
+        confirme = prompt("Finalizar Cadastro - S para sim ou N - Não?");
+        if (confirme.toUpperCase() == "S") {
+            x = "S";
+        };
+    
+        document.getElementById("relatorio").innerHTML = "<p>" + relatorio + "<p>";
+    }
+}
+
+/* Função cadastrar produto */
+
+function cadastrar_produto() {
+    let x = "n";
+    relatorio = "*- Relatório de Produto -* <br> ";
+
+    while (x.toUpperCase() != "S") {
+        id = parseInt(prompt("Digite o codigo do produto"));
+        nome = prompt("Digite o nome do Produto");
+        marca = prompt("Digite a marca do produto");
+        localizacao = prompt("Digite a localização do produto");
+
+        relatorio = relatorio + "<br>+-----------------------------------------------------------+<br><br>"+
+        "ID : " + id + "<br>" +
+        "NOME : " + nome + "<br>"+
+        "MARCA : " + marca + "<br>"+
+        "LOCALIZAÇÃO : " + localizacao + "<br>+-----------------------------------------------------------+<br><br>"
+    
+        confirme = prompt("Finalizar Cadastro - S para sim ou N - Não?");
+        if (confirme.toUpperCase() == "S") {
+            x = "S";
+        };
+    
+        document.getElementById("relatorio").innerHTML = "<p>" + relatorio + "<p>";
+    }
 }
